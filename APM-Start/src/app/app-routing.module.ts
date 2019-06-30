1f39d3a07904a101fa1831ba808233ab137cbf1a
Vartika06 <magicalvartika06@gmail.com>
2019-06-30 11:51:20 +0530

    Lazy Loading

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 const routePaths = [
   { path: 'welcome', component: WelcomeComponent },
+  {
+    path: 'products',
+    canActivate: [AuthGuard],
+    data: { preload: false },
+    loadChildren: './products/product.module#ProductModule'},
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routePaths, {enableTracing: true})
+    RouterModule.forRoot(routePaths,
+      {preloadingStrategy: SelectiveStrategy})
   ],
   exports: [RouterModule]
 })
